ae44051626516ee51e84c1c8fc2ad4aec9702536
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 18:09:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :roster, :student_name, :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+        @roster[grade] << student_name
+    else
+        @roster[grade] = []
+        @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort
+
+    end
+  end
+
+end
